37434988366c46505d02ea7409ec10cf2b57ea7f
lotfisilami <lotfisilami@gmail.com>
2018-08-01 14:57:24 +0100

    phpunit...

--- src/AppTest.php
+++ src/AppTest.php
@@ -1,12 +1,11 @@
 <?php
 
-/**
- * Created by PhpStorm.
- * User: LOTFI
- * Date: 01/08/2018
- * Time: 11:44
- */
+
 class AppTest
 {
+
+    public static function getNeverMindClass(){
+        return new nevermind();
+    }
 
 }
--- tests/NevermindTest.php
+++ tests/NevermindTest.php
@@ -1,12 +1,34 @@
 <?php
 
-/**
- * Created by PhpStorm.
- * User: LOTFI
- * Date: 31/07/2018
- * Time: 17:22
- */
-class NevermindTest
+
+use PHPUnit\Framework\TestCase;
+
+
+class NevermindTest extends TestCase
 {
+          public $NM;
+
+
+    public function setUp() {
+        $this->NM = AppTest::getNeverMindClass();
+    }
+
+
+
+
+    public function testSend_start(){
+        $json_result = $this->NM->send_start();
+        $result = json_decode($json_result,true);
+
+        $this->assertArrayHasKey('size', $result);
+        $this->assertArrayHasKey('quizz_id', $result);
+
+    }
+
+
+
+
 
 }
+
+
