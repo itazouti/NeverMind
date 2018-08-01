c27e1fe2a909c218736376ef731022d9938ee3d3
lotfisilami <lotfisilami@gmail.com>
2018-08-01 15:43:30 +0100

    codecovgi

--- tests/NevermindTest.php
+++ tests/NevermindTest.php
@@ -16,12 +16,12 @@ class NevermindTest extends TestCase
 
 
 
-    public function testSend_start(){
+    public function Send_start(){
         $json_result = $this->NM->send_start();
         $result = json_decode($json_result,true);
 
-        //$this->assertArrayHasKey('size', $result);
-        //$this->assertArrayHasKey('quizz_id', $result);
+        $this->assertArrayHasKey('size', $result);
+        $this->assertArrayHasKey('quizz_id', $result);
 
     }
 
