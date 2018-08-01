c98320a32d701a6ba1a6d8a81c1bd0c521f61951
lotfisilami <lotfisilami@gmail.com>
2018-08-01 14:47:25 +0100

    phpunit

---
+++ src/AppTest.php
@@ -0,0 +1,12 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: LOTFI
+ * Date: 01/08/2018
+ * Time: 11:44
+ */
+class AppTest
+{
+
+}
---
+++ tests/NevermindTest.php
@@ -0,0 +1,12 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: LOTFI
+ * Date: 31/07/2018
+ * Time: 17:22
+ */
+class NevermindTest
+{
+
+}
